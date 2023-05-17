d5c18b0ce1ba947db46049de01ae071ff100ef0d
Marcin <marcin.kurzydlak@gmail.com>
2023-05-17 22:35:35 +0200

    Add new files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Marcin Kurzydlak, nr albumu: 118614</h1>
+      <div>
+        <img src="https://storage.opinieouczelniach.pl/source/school/logo/1/P_fVKBE9yj7QA7Wym8sPIme1M10zUt98.png?fm=pjpg&w=300&h=200&fit=crop" alt="Logo SAN" />
+      </div>
+    </>
+  )
+}
+
+export default App
